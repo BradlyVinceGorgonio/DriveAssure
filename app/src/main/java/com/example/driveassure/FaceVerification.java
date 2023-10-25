822f351623192e104d4db7bad9408ded37157cf2
DanielGals <janjangallaron@gmail.com>
2023-10-26 04:00:58 +0800

    assasda

--- app/src/main/java/com/example/driveassure/FaceVerification.java
+++ app/src/main/java/com/example/driveassure/FaceVerification.java
@@ -3,12 +3,48 @@ package com.example.driveassure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.camera2.CameraCharacteristics;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.OutputConfiguration;
+import android.hardware.camera2.params.SessionConfiguration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.Surface;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Arrays;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class FaceVerification extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private CameraManager cameraManager;
+    private String cameraId;
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private CameraCaptureSession captureSession;
+    private SurfaceView surfaceView;
+    private CircleImageView imageViewer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +54,7 @@ public class FaceVerification extends AppCompatActivity {
 
 
 
-       
+
 
 // Now you have access to the data in the new activity.
 
@@ -46,6 +82,122 @@ public class FaceVerification extends AppCompatActivity {
         });
 
 
+        surfaceView = findViewById(R.id.surfaceView);
+        //imageViewer = findViewById(R.id.imageViewer);
+
+        ImageButton captureButton = findViewById(R.id.captureButton);
+        captureButton.setOnClickListener(v -> {
+            // Capture image here
+            //captureImage();
+        });
+
+        // Check and request camera permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        } else {
+            openCamera();
+        }
 
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(this, "Camera permission is required to use the camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+    private void openCamera() {
+        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        String frontCameraId = null;
+
+        try {
+            for (String cameraId : cameraManager.getCameraIdList()) {
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                    frontCameraId = cameraId; // Store the front camera ID
+                    break;
+                }
+            }
+
+            if (frontCameraId != null) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    cameraManager.openCamera(frontCameraId, cameraStateCallback, null);
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+                }
+            } else {
+                // Handle the case where a front camera is not available on the device.
+                Toast.makeText(this, "Front camera not available on this device.", Toast.LENGTH_SHORT).show();
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private final CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            createCameraPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+    private void createCameraPreview() {
+        try {
+            Surface surface = surfaceView.getHolder().getSurface();
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(surface);
+
+            cameraDevice.createCaptureSession(
+                    Arrays.asList(surface),
+                    new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            if (cameraDevice == null) {
+                                return;
+                            }
+                            captureSession = session;
+                            try {
+                                captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                            Toast.makeText(FaceVerification.this, "Failed to create camera preview.", Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    null
+            );
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
 }
