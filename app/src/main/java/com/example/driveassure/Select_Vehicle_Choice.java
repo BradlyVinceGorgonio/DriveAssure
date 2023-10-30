1754383a29a46f7e737552377df1af9d0cf9c832
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-10-31 01:10:14 +0800

    Selece New Vehicle

---
+++ app/src/main/java/com/example/driveassure/Select_Vehicle_Choice.java
@@ -0,0 +1,14 @@
+package com.example.driveassure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select_Vehicle_Choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_vehicle_choice);
+    }
+}
