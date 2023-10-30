50471be9fa09026e2a0a4c9855096de6c04b98c2
DanielGals <janjangallaron@gmail.com>
2023-10-30 22:21:29 +0800

    taetae

--- app/src/main/java/com/example/driveassure/homeUserFragment.java
+++ app/src/main/java/com/example/driveassure/homeUserFragment.java
@@ -3,19 +3,40 @@ package com.example.driveassure;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class homeUserFragment extends Fragment {
 
+    ImageButton chatMsg;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_home_user, container, false);
+
+
+        chatMsg = view.findViewById(R.id.chatMsg);
+        chatMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new instance of the fragment you want to navigate to
+                ChatList fragment2 = new ChatList();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.pagerMain, fragment2);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+
         return view;
     }
 }
--- app/src/main/java/com/example/driveassure/terms_and_agreement.java
+++ app/src/main/java/com/example/driveassure/terms_and_agreement.java
@@ -2,13 +2,58 @@ package com.example.driveassure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class terms_and_agreement extends AppCompatActivity {
-
+CheckBox AgreeTermsandAgreement;
+Button confirmAgreeBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_agreement);
+
+        // Retrieve the string from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        // Vehicle Type
+
+        
+
+        AgreeTermsandAgreement = findViewById(R.id.AgreeTermsandAgreement);
+        confirmAgreeBtn = findViewById(R.id.confirmAgreeBtn);
+
+        // Initially, set the button to disabled and gray
+        confirmAgreeBtn.setEnabled(false);
+        confirmAgreeBtn.setBackgroundColor(getResources().getColor(R.color.disabledGrey));
+
+        AgreeTermsandAgreement.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // If the checkbox is checked, enable the button and set it to blue
+                    confirmAgreeBtn.setEnabled(true);
+                    confirmAgreeBtn.setBackgroundColor(getResources().getColor(R.color.blue));
+                } else {
+                    // If the checkbox is not checked, disable the button and set it to gray
+                    confirmAgreeBtn.setEnabled(false);
+                    confirmAgreeBtn.setBackgroundColor(getResources().getColor(R.color.disabledGrey));
+                }
+            }
+        });
+
+        confirmAgreeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(terms_and_agreement.this, CreatingListingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
