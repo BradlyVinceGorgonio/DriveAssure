313ff004f436bf94dde54e54f8f9f6d4c5d26a7e
DanielGals <janjangallaron@gmail.com>
2023-11-08 03:51:40 +0800

    Done on uploading to firebase
    Lets go with adding next page then going back

--- app/src/main/java/com/example/driveassure/CreatingListingActivity.java
+++ app/src/main/java/com/example/driveassure/CreatingListingActivity.java
@@ -30,9 +30,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -72,11 +76,13 @@ public class CreatingListingActivity extends AppCompatActivity {
     String[] conditions = {"Brand new", "Good as new", "Good", "Used"};
     String[] transmissions = {"Automatic", "Manual"};
 
-    String selectedMotorcycleBrand;
-    String selectedCarBrand;
+    String selectedBrand;
+
     String selectedTransmission;
     String selectedFuelType;
     String selectedCondition;
+
+
 
 
     @Override
@@ -86,6 +92,7 @@ public class CreatingListingActivity extends AppCompatActivity {
 
         // Add this to your onCreate method or the Application class
         FirebaseApp.initializeApp(this);
+        deleteImagesInDirectory();
 
 
         // Retrieve the string from the intent
@@ -93,6 +100,7 @@ public class CreatingListingActivity extends AppCompatActivity {
         String message = intent.getStringExtra("EXTRA_MESSAGE");
         // Vehicle Type
         Log.d("POWERPOWER", "Message nasa listing act na: " + message);
+
 
 
 
@@ -107,7 +115,7 @@ public class CreatingListingActivity extends AppCompatActivity {
         conditionAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedCondition = (String) parent.getItemAtPosition(position);
+               selectedCondition = (String) parent.getItemAtPosition(position);
                // Toast.makeText(CreatingListingActivity.this, "Selected Condition: " + selectedCondition, Toast.LENGTH_LONG).show();
             }
         });
@@ -123,7 +131,7 @@ public class CreatingListingActivity extends AppCompatActivity {
         fuelTypeAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedFuelType = (String) parent.getItemAtPosition(position);
+                selectedFuelType = (String) parent.getItemAtPosition(position);
 
             }
         });
@@ -136,41 +144,38 @@ public class CreatingListingActivity extends AppCompatActivity {
         transmissionAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedTransmission = (String) parent.getItemAtPosition(position);
+                selectedTransmission = (String) parent.getItemAtPosition(position);
 
             }
         });
 
 
-        TextInputLayout carBrandTextInputLayout = findViewById(R.id.textInputLayoutCarBrand);
+
         AutoCompleteTextView carBrandAutoComplete = findViewById(R.id.carBrandSpinner);
 
+
+
         ArrayAdapter<String> carBrandsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, carBrands);
+        ArrayAdapter<String> motorcycleBrandsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, motorcycleBrands);
+
         carBrandAutoComplete.setAdapter(carBrandsAdapter);
 
+
+        if (message.equals("Car")) {
+            carBrandAutoComplete.setAdapter(carBrandsAdapter);
+        } else {
+            carBrandAutoComplete.setAdapter(motorcycleBrandsAdapter);
+        }
 
         carBrandAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedCarBrand = (String) parent.getItemAtPosition(position);
-                // Handle the selection as needed
+                selectedBrand = (String) parent.getItemAtPosition(position);
             }
         });
 
 
-        TextInputLayout motorCycleTextInputLayout = findViewById(R.id.textInputLayoutMotorCycle);
-        AutoCompleteTextView motorCycleAutoComplete = findViewById(R.id.motorcycleBrandSpinner);
 
-        ArrayAdapter<String> motorCycleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, motorcycleBrands);
-        motorCycleAutoComplete.setAdapter(motorCycleAdapter);
-
-        motorCycleAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedMotorCycleBrand = (String) parent.getItemAtPosition(position);
-
-            }
-        });
 
 
        // Spinner motorcycleBrandsSpinner = findViewById(R.id.motorcycleBrandsSpinner);
@@ -283,6 +288,15 @@ public class CreatingListingActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(imageSlider, true);
 
 
+        Button submitBtn = findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImagesAndFieldsToFirebase();
+            }
+        });
+
+
     }
     private void openGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -344,25 +358,68 @@ public class CreatingListingActivity extends AppCompatActivity {
 
     private void uploadImagesAndFieldsToFirebase()
     {
+        TextInputEditText titleEditText = findViewById(R.id.titleHead);
+        String titleText = titleEditText.getText().toString();
+
+        TextInputEditText ModelEditText = findViewById(R.id.vehicleModel);
+        String modelText = ModelEditText.getText().toString();
+
+        TextInputEditText vehicleReleaseDate = findViewById(R.id.vehicleReleaseDate);
+        String vehicleReleaseDateText = vehicleReleaseDate.getText().toString();
+
+        TextInputEditText vehicleSeatCount = findViewById(R.id.vehicleSeatCount);
+        String vehicleSeatCountText =vehicleSeatCount.getText().toString();
+
+        TextInputEditText vehicleColour = findViewById(R.id.vehicleColour);
+        String vehicleColourText =vehicleColour.getText().toString();
+
+        TextInputEditText vehicleKilometerRun = findViewById(R.id.vehicleKilometerRun);
+        String vehicleKilometerRunText = vehicleKilometerRun.getText().toString();
+
+        TextInputEditText vehicleAddress = findViewById(R.id.vehicleAddress);
+        String vehicleAddressText = vehicleAddress.getText().toString();
+
+        TextInputEditText vehicleDescription = findViewById(R.id.vehicleDescription);
+        String vehicleDescriptionText = vehicleDescription.getText().toString();
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        // Initialize Firebase Authentication
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
+        // Check if there is a currently signed-in user
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        String uid = currentUser.getUid();
         // Retrieve the string from the intent
         Intent intent = getIntent();
         String carType= intent.getStringExtra("EXTRA_MESSAGE");
         // Car Type ^^
 
-        Map<String, Object> carData = new HashMap<>();
-        carData.put("vehicle type", carType);
-        carData.put("selectedMotorcycleBrand", selectedMotorcycleBrand);
-        carData.put("selectedCarBrand", selectedCarBrand);
-        carData.put("selectedTransmission", selectedTransmission);
-        carData.put("selectedFuelType", selectedFuelType);
-        carData.put("selectedCondition", selectedCondition);
-
-
         DocumentReference carPostDocument = db.collection("car-posts").document(); // Automatically generates a unique ID
         String carPostId = carPostDocument.getId(); // Get the ID
+
+
+        Map<String, Object> carData = new HashMap<>();
+        carData.put("Vehicle Type", carType);
+        carData.put("Vehicle Brand", selectedBrand);
+        carData.put("Vehicle Transmission", selectedTransmission);
+        carData.put("Vehicle Fuel Type", selectedFuelType);
+        carData.put("Vehicle Condition", selectedCondition);
+        carData.put("Vehicle post-id", carPostId);
+        carData.put("Vehicle Title", titleText);
+        carData.put("Vehicle Model", modelText);
+        carData.put("Vehicle Year Model", vehicleReleaseDateText);
+        carData.put("Vehicle Seat Count", vehicleSeatCountText);
+        carData.put("Vehicle Color", vehicleColourText);
+        carData.put("Vehicle Kilometer", vehicleKilometerRunText);
+        carData.put("Vehicle Address", vehicleAddressText);
+        carData.put("Vehicle Description", vehicleDescriptionText);
+        carData.put("uid",uid );
+
+
+
+
 
         carPostDocument.set(carData)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -371,6 +428,8 @@ public class CreatingListingActivity extends AppCompatActivity {
                         // Car-post document added successfully
                         // You have the carPostId as the document's ID
                         // Now create a subcollection with the same ID
+
+                        Log.d("PANOKOTOGA", "Car listing done");
                         createSubcollectionWithSameId(db, carPostId, carData);
                     }
                 })
@@ -398,6 +457,8 @@ public class CreatingListingActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void aVoid) {
                         // Subcollection document added successfully with the same ID
+                        Log.d("PANOKOTOGA", "Create Listing User done");
+                        uploadImagesToFirebaseStorage(carPostId);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -406,6 +467,71 @@ public class CreatingListingActivity extends AppCompatActivity {
                         // Handle errors
                     }
                 });
+    }
+
+    private static final String TAG = "ImageUploader";
+
+    public void uploadImagesToFirebaseStorage(String uid) {
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File carPicsDir = new File(downloadsDir, "carPics");
+
+        if (carPicsDir.exists() && carPicsDir.isDirectory()) {
+            File[] imageFiles = carPicsDir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    String lowercaseName = name.toLowerCase();
+                    return lowercaseName.endsWith(".jpg") || lowercaseName.endsWith(".png");
+                }
+            });
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference().child("carposts").child(uid);
+
+            for (File imageFile : imageFiles) {
+                Uri imageUri = Uri.fromFile(imageFile);
+                String imageName = imageFile.getName();
+
+                StorageReference imageRef = storageRef.child(imageName);
+
+                UploadTask uploadTask = imageRef.putFile(imageUri);
+
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("PANOKOTOGA", "Image uploaded successfully");
+                    }
+                });
+            }
+        } else {
+            Log.e(TAG, "The 'carPics' directory does not exist or is not a directory.");
+            // Handle this case accordingly.
+        }
+    }
+
+    public void deleteImagesInDirectory() {
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File carPicsDir = new File(downloadsDir, "carPics");
+
+        if (carPicsDir.exists() && carPicsDir.isDirectory()) {
+            File[] imageFiles = carPicsDir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    String lowercaseName = name.toLowerCase();
+                    return lowercaseName.endsWith(".jpg") || lowercaseName.endsWith(".png");
+                }
+            });
+
+            for (File imageFile : imageFiles) {
+                if (imageFile.delete()) {
+                    // File deleted successfully
+                } else {
+                    // Failed to delete the file, handle this case accordingly
+                }
+            }
+        } else {
+            // The 'carPics' directory does not exist or is not a directory.
+            // Handle this case accordingly.
+        }
     }
 
 }
