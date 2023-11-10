ab1bd1231d880904417690132033e8b8e3453513
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-11-11 03:43:10 +0800

    homepage slider ads

--- app/src/main/java/com/example/driveassure/homeUserFragment.java
+++ app/src/main/java/com/example/driveassure/homeUserFragment.java
@@ -3,6 +3,8 @@ package com.example.driveassure;
 // homeUserFragment
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Slide;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +22,15 @@ import android.widget.Spinner;
 import android.widget.AdapterView;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -64,15 +70,50 @@ public class homeUserFragment extends Fragment implements HomeListingCarAdapter.
 
     private List<HomeListingClass> HistoryList;
     private HomeListingCarAdapter homeListingCarAdapter;
-
     ProgressBar progressBarID2;
 
+    ViewPager2 viewpager2;
 
+    private Handler slideHandler = new Handler();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_user, container, false);
 
+        viewpager2 = view.findViewById(R.id.viewPager);
+        List<SlideItem> sliderItem = new ArrayList<>();
+        sliderItem.add(new SlideItem(R.drawable.slider1));
+        sliderItem.add(new SlideItem(R.drawable.slider2));
+        sliderItem.add(new SlideItem(R.drawable.slider3));
+        sliderItem.add(new SlideItem(R.drawable.slider4));
+        sliderItem.add(new SlideItem(R.drawable.slider5));
+
+        viewpager2.setAdapter(new SlideAdapter(sliderItem, viewpager2));
+
+        viewpager2.setClipToPadding(false);
+        viewpager2.setClipChildren(false);
+        viewpager2.setOffscreenPageLimit(5);
+        viewpager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(30));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+            float r = 1- Math.abs(position);
+            page.setScaleY(0.05f + r*0.15f);
+            }
+        });
+
+        viewpager2.setPageTransformer(compositePageTransformer);
+        viewpager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                slideHandler.removeCallbacks(sliderRunnable);
+                slideHandler.postDelayed(sliderRunnable, 2000);
+            }
+        });
 
         progressBarID2 = view.findViewById(R.id.progressBarID2);
 
@@ -135,6 +176,7 @@ public class homeUserFragment extends Fragment implements HomeListingCarAdapter.
 
 
 
+
         chatMsg = view.findViewById(R.id.chatMsg);
         chatMsg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -155,6 +197,14 @@ public class homeUserFragment extends Fragment implements HomeListingCarAdapter.
 
         return view;
     }
+
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewpager2.setCurrentItem(viewpager2.getCurrentItem() + 1);
+        }
+    };
     @Override
     public void onItemClick(HomeListingClass history) {
         String historyUid = history.getUid(); // Assuming HomeListingClass has a getUid() method
