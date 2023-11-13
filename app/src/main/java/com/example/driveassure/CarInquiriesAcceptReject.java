27f5070ee0f98291f75468d236d2b99c9f6f9b38
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-11-14 02:20:34 +0800

    Car_inquiries_Accept_reject

---
+++ app/src/main/java/com/example/driveassure/CarInquiriesAcceptReject.java
@@ -0,0 +1,14 @@
+package com.example.driveassure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarInquiriesAcceptReject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_inquiries_accept_reject);
+    }
+}
