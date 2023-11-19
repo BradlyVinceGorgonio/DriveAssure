0587a3a4f45af32aaf2347ab923b2e31567980a7
DanielGals <janjangallaron@gmail.com>
2023-11-19 18:34:10 +0800

    asdasd

--- app/src/main/java/com/example/driveassure/profileUserFragment.java
+++ app/src/main/java/com/example/driveassure/profileUserFragment.java
@@ -13,12 +13,19 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class profileUserFragment extends Fragment {
     CardView carRentingCardView;
     ImageView favoritesImage;
+    CircleImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +33,7 @@ public class profileUserFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile_user, container, false);
 
+        imageView = view.findViewById(R.id.imageView);
         ImageView logout = view.findViewById(R.id.logoutBtn);
 
         logout.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +82,35 @@ public class profileUserFragment extends Fragment {
             }
         });
 
+        LoadProfileImage();
+
+
         return view;
+    }
+
+    private void LoadProfileImage()
+    {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        String userId = user.getUid();
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("users/" + userId + "/face.jpg");
+
+        final String[] tempProfilePictureUrl = {"face.jpg"};  // Declare a final temporary variable
+
+        storageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+            tempProfilePictureUrl[0] = uri.toString();  // Assign the value to the temporary variable
+            String storageUrl = uri.toString();
+            // Use Glide to load the image into the ImageView
+            Glide.with(this)
+                    .load(storageUrl)
+                    .placeholder(R.drawable.personvector)  // Placeholder image while loading (optional)
+                    .error(R.drawable.personvector)       // Error image if the loading fails (optional)
+                    .into(imageView);
+
+        }).addOnFailureListener(e -> {
+            // Handle any errors that occur while fetching the profile picture URL
+        });
+
     }
 }
