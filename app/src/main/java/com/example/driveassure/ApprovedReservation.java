2f957c3aa8a485e3cd5d031f80ed22b917a33730
DanielGals <janjangallaron@gmail.com>
2023-11-19 19:14:58 +0800

    Jopayyy

--- app/src/main/java/com/example/driveassure/ApprovedReservation.java
+++ app/src/main/java/com/example/driveassure/ApprovedReservation.java
@@ -1,14 +1,28 @@
 package com.example.driveassure;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,6 +32,38 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ApprovedReservation extends AppCompatActivity {
 
@@ -31,7 +77,10 @@ public class ApprovedReservation extends AppCompatActivity {
     ImageView uploadedPayment;
     ProgressBar ProgressBarFaceId;
     TextView onTheDayPayment;
+    Button approvedButton;
+    TextView textView25;
 
+    private static final String CHANNEL_ID = "DownloadChannel";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +98,28 @@ public class ApprovedReservation extends AppCompatActivity {
         uploadedPayment = findViewById(R.id.uploadedPayment);
         ProgressBarFaceId = findViewById(R.id.ProgressBarFaceId);
         onTheDayPayment = findViewById(R.id.onTheDayPayment);
+        approvedButton = findViewById(R.id.approvedBtn);
+        textView25 = findViewById(R.id.textView25);
+
+        createNotificationChannel();
+        textView25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadDocx(view);
+                Toast.makeText(getApplicationContext(),"Contract Downloaded", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
+
+
+        approvedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ApprovedReservation.this, ProcessingReservation.class);
+                startActivity(intent);
+            }
+        });
 
 
 
@@ -137,4 +206,101 @@ public class ApprovedReservation extends AppCompatActivity {
                     }
                 });
     }
+
+    public void downloadDocx(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            new ApprovedReservation.DownloadFileTask(this).execute();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private static class DownloadFileTask extends AsyncTask<Void, Void, Void> {
+        private Context context;
+
+        DownloadFileTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            InputStream inputStream = context.getResources().openRawResource(R.raw.ownercontract);
+            File outputDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File outputFile = new File(outputDir, "carowneragreement.docx");
+
+            try {
+                if (!outputDir.exists()) {
+                    outputDir.mkdirs();
+                }
+
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead);
+                }
+
+                bufferedInputStream.close();
+                fileOutputStream.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Handle the exception, e.g., display a toast message
+                publishProgress(); // Inform onPostExecute about the error
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            showDownloadCompleteNotification();
+        }
+
+        private void showDownloadCompleteNotification() {
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.heart_icon) // Use your custom small icon
+                    .setContentTitle("Download Complete")
+                    .setContentText("Rental Agreement.docx file downloaded")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            notificationManager.notify(1, builder.build());
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            Toast.makeText(context, "Error downloading file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Download Channel";
+            String description = "Channel for download notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
